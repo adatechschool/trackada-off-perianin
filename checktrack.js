40e23770202c670f325b21e5f4847602c14498bb
Offichigo <oph.perianin@gmail.com>
2026-02-25 10:17:34 +0100

    Vérifier que les projets sont bien initialisés comme des projets git check

--- checktrack.js
+++ checktrack.js
@@ -7,29 +7,40 @@ import { homedir } from "os";
 //===================================================================================================================
 const data = readFileSync("track.json", "utf-8");
 const track = JSON.parse(data);
-console.log(track);
-//Vérifier que le dossier ada existe dans le dossier home/Documents (~)
+// console.log(track);
 //===================================================================================================================
+//Vérifier que le dossier ada existe dans le dossier home/Documents (~)
 const home = homedir();
 const ada = join(home, "documents", "ada");
 if (existsSync(ada)) {
-  console.log("✅ : le dossier ada est là !");
+  console.log("✅ dossier ada");
 } else {
-  console.log("❌ : le dossier ada n'est pas là !");
+  console.log("❌ dossier ada");
 }
+//====================================================================================================================
 // Vérifier que les dossiers des projets existent au bon endroit et sont correctement nommés
 //Première erreur possible, lorsque le dossier du projet n’existe pas ou est mal nommé :
-//- le dossier n'existe pas ou n'est pas nommé correctement
+//le dossier n'existe pas ou n'est pas nommé correctement
 // Vérifier que les projets sont bien initialisés comme des projets git
-
-//====================================================================================================================
 track.projects.forEach((project) => {
   const findproject = join(ada, project.name);
   const findgit = join(ada, project.name, ".git");
-  if (existsSync(findproject, findgit)) {
+  if (existsSync(findproject)) {
     console.log("✅ dossier " + " " + project.name);
+  } else if ((track, project.name)) {
+    console.log(
+      "- le dossier " +
+        project.name +
+        " n'existe pas ou n'est pas nommé correctement",
+    );
   } else {
     console.log("❌ dossier " + " " + project.name);
   }
+  if (existsSync(findgit)) {
+    console.log("✅ le repository git est bien initialisé");
+  } else {
+    console.log("- le repository git n'est pas initialisé");
+  }
 });
+
 //=====================================================================================================================
